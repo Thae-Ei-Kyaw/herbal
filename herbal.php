e95a744e458da146b19d714f8335a9f2017df690
Thae Ei Kyaw <thaeeikyaw39@gmail.com>
2020-10-24 23:41:01 +0630

    second commit

---
+++ herbal.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Hsp</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shorcut icon" href="images/48x48 px - png-logo.png">
+	
+</head>
+<body>
+	<?php include_once "nav.php"; ?>
+	<section id="herbal">
+		<div class="container">
+			<div class="row my-5">
+				<div class="col-lg-9 col-md-6">
+					<div class="mt-5">
+						<h4 class="text-center font-rb borderimg">Herbal Paradise</h4>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 py-2 mt-3">
+					<img src="images/Post-1-Introduction Post.jpg" class="img-fluid" alt="">
+				</div>
+			</div>
+			
+			<div class="row my-5">
+				<div class="col-lg-3 col-md-3">
+					<img src="images/1-Tiens Nutrient Super Calcium Power.jpg" class="img-fluid mb-3" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3">
+					<img src="images/2-Tiens Nutrient Super Calcium Powder for children.jpg" class="img-fluid mb-3" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3">
+					<img src="images/3-Tiens Super Calcium Powder with Metabolic Factors (1).jpg" class="img-fluid mb-3" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3">
+					<img src="images/4-Tiens Super Calicium Capsules Lecithin.jpg" class="img-fluid" alt="">
+				</div>
+			</div>
+			<div class="row my-5">
+				<div class="col-lg-3 col-md-3">
+					<img src="images/5-Tiens Zinc Capsules ..jpg" class="img-fluid mb-3" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3">
+					<img src="images/8-Sprirulina.jpg" class="img-fluid mb-3" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3">
+					<img src="images/12-Tiens Health Mattress.jpg" class="img-fluid mb-3" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3">
+					<img src="images/13-Tiens Health Pillow.jpg" class="img-fluid" alt="">
+				</div>
+			</div>
+			<div class="row my-5">
+				<div class="col">
+					<video width="350" height="200" class="mb-3" controls>
+	                       <source src="images/Post-2-What is Health.mp4" type="video/mp4">    
+	                </video>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<?php include_once "footer.php"; ?>
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+
+	
+	
+</body>
+</html>
